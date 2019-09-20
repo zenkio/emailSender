a543082140b3e06865dcaa6da63ecc1cbfaa8028
zenkio <zenkio.it@gmail.com>
2019-09-20 11:59:45 +0800

    init

---
+++ src/test/java/com/zenkio/emailSender/EmailSenderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zenkio.emailSender;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailSenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
